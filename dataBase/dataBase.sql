6786fb1fdd8dabeb2c0c83dcb29606078a4560f3
https://github.com/dusanivkovic <ivdule77@gmail.com>
2024-03-18 12:35:43 +0100

    Add sql query for create Table Product

--- dataBase/dataBase.sql
+++ dataBase/dataBase.sql
@@ -7,3 +7,20 @@ CREATE TABLE users (
     )
 
 INSERT INTO `users`(`user_id`, `firstname`, `lastname`, `mail`, `password`) VALUES ('1','Dusan','Ivkovic','dusan.ivkovic@skolers.org','12345678')
+
+CREATE TABLE `product` (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    price FLOAT NOT NULL,
+    availableQuantity INT
+    )
+
+INSERT INTO `product`(`id`, `title`, `price`, `availableQuantity`) VALUES (
+'Kempa Kempa GOLD Shorts, schwarz, XXS, 200319791',	'165',	'10',
+'Kempa Kempa GOLD Shorts, schwarz, XXS, 200319801',	'138',	'10',
+'Kempa WASSERFLASCHE 750 ml, transparent, 200120902', '138',	'10',
+'Kempa KEMPA HANDBALLWAX 200 ml, 200158302',	'138',	'10',
+'Kempa KEMPA HANDBALLWAXENTFERNER, 200158502', '138',	'10',
+'Kempa CORE T-Shirt, kempa blau, XXL, 200215102',	'78',	'10',
+'Kempa CORE T-Shirt, kempa blau, XXXL, 200215102',	'78',	'10'
+)
